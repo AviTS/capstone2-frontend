9e6b29b13f9d859ea6dfabbe02b9f9ed107a1276
AviTS <avisabath@gmail.com>
2022-07-26 14:50:08 -0400

    library js updated

--- src/libraries/Library.js
+++ src/libraries/Library.js
@@ -11,24 +11,44 @@ function Library() {
 
   const { library_id } = useParams();
 
-  useEffect(
-    function getBooks() {
-      async function getBook() {
-        let books = await BookAppApi.getBooksFromLib(library_id);
-        setBooks(books);
-      }
-      getBook();
-    },
-    [library_id]
-  );
+  useEffect(function getBooks() {
+    async function getBook() {
+      let books = await BookAppApi.getBooksFromLib(library_id);
+      setBooks(books);
+    }
+    getBook();
+  }, []);
 
   console.log(books);
+
+  let booksList = [];
+
+  if (books) {
+    for (let i = 0; i < books.length; i++) {
+      let bookId = books[i].book_id;
+      let bookObj = {
+        id: bookId,
+      };
+
+      booksList.push(bookObj);
+    }
+  }
 
   if (!books) return;
 
   return (
     <div className="LibBookList">
-      <h1>{books}</h1>
+      {books ? (
+        <div className="BookList-results">
+          <ul>
+            {booksList.map((b) => (
+              <li key={b.id}>{b.id}</li>
+            ))}
+          </ul>
+        </div>
+      ) : (
+        <p>No results</p>
+      )}
     </div>
   );
 }
